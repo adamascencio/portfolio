381a25c8ab5aa91a56c4926cd526adf34e43329f
adamascencio <ascencioadam@gmail.com>
2022-11-04 11:31:23 -0700

    add links to github repos

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -77,12 +77,12 @@ const StyledText = styled.div`
     font-size: clamp(var(--fs-sm),4vw,var(--fs-lg));
   }
 
-  span:last-child {
+  span:last-child, span:nth-child(2) {
     color: var(--white);
     text-decoration: underline;
   }
 
-  span:last-child:hover {
+  span:last-child:hover, span:nth-child(2):hover {
     color: var(--green);
     transform: scale(1.02);
   }
@@ -106,21 +106,24 @@ export default function Projects() {
       description: 'A platform to find and create pick-up games in your local community.',
       tech: ['MongoDB', 'Express', 'React', 'Node.js', 'Google Maps API'],
       src: 'https://i.imgur.com/0ZgfHL4.png',
-      href: 'https://home-court-cap.herokuapp.com/'
+      href: 'https://home-court-cap.herokuapp.com/',
+      github: 'https://github.com/adamascencio/HomeCourt'
     },
     {
       name: 'Power List', 
       description: 'A tool for managing important tasks',
       tech: ['MongoDB', 'Express', 'Node.js'],
       src: 'https://i.imgur.com/R6FeFdz.png', 
-      href: 'https://the-power-list.herokuapp.com/'
+      href: 'https://the-power-list.herokuapp.com/',
+      github: 'https://github.com/adamascencio/The-Power-List'
     },
     {
       name: 'Blackjack', 
       description: 'A simple game of Blackjack',
       tech: ['HTML', 'CSS', 'JavaScript'],
       src: 'https://i.imgur.com/Ipi0qn0.png', 
-      href: 'https://adamascencio.github.io/Blackjack/'
+      href: 'https://adamascencio.github.io/Blackjack/',
+      github: 'https://github.com/adamascencio/Blackjack'
     }
   ];
 
@@ -134,6 +137,7 @@ export default function Projects() {
         <StyledText>
           <span>{project.tech.join(', ')}</span>
           <span><a href={project.href} target='_blank' rel='noopener noreferrer'>View Deployed App</a></span>
+          <span><a href={project.github} target='_blank' rel='noopener noreferrer'>View Github Repo</a></span>
         </StyledText>
         <StyledWrapperDiv>
           <StyledImg src={project.src} alt={project.name} />
